235021b8a6606145a1b74f024e391cf679f841c6
Martichou <martichou.andre@gmail.com>
2017-10-05 17:35:08 +0200

    build: update build version

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ KERNEL_DEFCONFIG=potter_defconfig
 DTBTOOL=$KERNEL_DIR/Dtbtool/
 JOBS=4
 ANY_KERNEL2_DIR=$KERNEL_DIR/AnyKernel2/
-FINAL_KERNEL_ZIP=Alize-8.0_v107.zip
+FINAL_KERNEL_ZIP=Alize-8.0_v108.zip
 # Speed up build process
 MAKE="./makeparallel"
 
